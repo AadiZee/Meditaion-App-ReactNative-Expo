11ba3df192130e841b0050727edec9b9c797c797
Adnan Zaib <adnanzaib6@gmail.com>
2024-09-19 18:20:40 +0500

    First and final commit

---
+++ components/Content.tsx
@@ -0,0 +1,8 @@
+import { SafeAreaView } from "react-native";
+import React from "react";
+
+const Content = ({ children }) => {
+  return <SafeAreaView className="flex-1 px-5 py-3">{children}</SafeAreaView>;
+};
+
+export default Content;
